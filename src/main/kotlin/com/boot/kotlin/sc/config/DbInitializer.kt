d50b3f82c7b74e7fb2eec2f2b80981ad950b5529
Sarawanan <sarawanan.mahalingam@icloud.com>
2022-03-24 08:28:49 +0530

    Initialize db

--- src/main/kotlin/com/boot/kotlin/sc/config/DbInitializer.kt
+++ src/main/kotlin/com/boot/kotlin/sc/config/DbInitializer.kt
@@ -4,14 +4,15 @@ import com.boot.kotlin.sc.entity.Customer
 import com.boot.kotlin.sc.entity.CustomerRepo
 import com.boot.kotlin.sc.entity.Product
 import com.boot.kotlin.sc.entity.ProductRepo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class DbInitializer {
-    @Bean
-    fun initialize(customerRepo: CustomerRepo, productRepo: ProductRepo) = ApplicationRunner {
+class DbInitializer @Autowired constructor(val customerRepo: CustomerRepo, val productRepo: ProductRepo) :
+    ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
         customerRepo.saveAndFlush(Customer("Sara", "Test address"))
         productRepo.saveAndFlush(Product("Rice", 10, 100.00))
         productRepo.saveAndFlush(Product("Wheat", 20, 200.00))
