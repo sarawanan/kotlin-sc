0102116c97fc33cc6e7009c2b90c33f408fb09b8
Sarawanan <sarawanan.mahalingam@icloud.com>
2022-03-24 08:01:45 +0530

    Initialize db

--- src/main/kotlin/com/boot/kotlin/sc/DbInitializer.kt
+++ src/main/kotlin/com/boot/kotlin/sc/DbInitializer.kt
@@ -1,6 +1,9 @@
 package com.boot.kotlin.sc
 
-import com.boot.kotlin.sc.entity.*
+import com.boot.kotlin.sc.entity.Customer
+import com.boot.kotlin.sc.entity.CustomerRepo
+import com.boot.kotlin.sc.entity.Product
+import com.boot.kotlin.sc.entity.ProductRepo
 import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -8,10 +11,7 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class DbInitializer {
     @Bean
-    fun initialize(
-        customerRepo: CustomerRepo, productRepo: ProductRepo,
-        cartRepo: CartRepo, cartItemRepo: CartItemRepo,
-    ) = ApplicationRunner {
+    fun initialize(customerRepo: CustomerRepo, productRepo: ProductRepo) = ApplicationRunner {
         customerRepo.saveAndFlush(Customer("Sara", "Test address"))
         productRepo.saveAndFlush(Product("Rice", 10, 100.00))
         productRepo.saveAndFlush(Product("Wheat", 20, 200.00))
